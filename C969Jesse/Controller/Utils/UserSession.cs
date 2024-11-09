3ae5deff64aeaf4623d372938b9852eb150e7af8
Yugandhar Borase <115479650+YugandharBorase@users.noreply.github.com>
2024-11-09 10:23:21 +0530

    Add files via upload

---
+++ C969Jesse/Controller/Utils/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C969Jesse.Utils
+{
+    public static class UserSession
+    {
+        public static string CurrentUserName { get; set; }
+        public static int CurrentUserId { get; set; }
+
+    }
+
+}
